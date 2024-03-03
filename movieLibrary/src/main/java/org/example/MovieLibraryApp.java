1081a8cef3e68e94afbbf9486b4911ede350a00f
Blazej Jaskula <blazejo93@gmail.com>
2024-03-03 12:40:58 +0100

    add movie library class

---
+++ movieLibrary/src/main/java/org/example/MovieLibraryApp.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.models.Movie;
+import org.example.models.MovieLibrary;
+
+public class MovieLibraryApp {
+    public static void main(String[] args) {
+
+        MovieLibrary movieLibrary = new MovieLibrary();
+        movieLibrary.fillMovieList();
+
+    }
+}
