45e8e73a8a7a837f8f78d230cec5893683810bac
Blazej Jaskula <blazejo93@gmail.com>
2024-03-03 13:04:05 +0100

    add switch

--- movieLibrary/src/main/java/org/example/MovieLibraryApp.java
+++ movieLibrary/src/main/java/org/example/MovieLibraryApp.java
@@ -1,13 +1,28 @@
 package org.example;
 
+import org.example.handlers.MenuHandlers;
 import org.example.models.Movie;
 import org.example.models.MovieLibrary;
+
+import static org.example.handlers.MenuOptions.*;
 
 public class MovieLibraryApp {
     public static void main(String[] args) {
 
         MovieLibrary movieLibrary = new MovieLibrary();
         movieLibrary.fillMovieList();
+        MenuHandlers.print();
+        switch(){
+            case DISPLAYINFORMATIONABOUTRANDOMMOVIE:
+                //TODO dopisać kod dla tej opcji
+                break;
+            case DISPLAYALLMOVIESFORACTUALACTOR:
+                //TODO dopisać kod dla tej opcji
+                break;
+            case QUIT:
+                System.out.println("Wychodzenie");
+                System.exit(1);
+        }
 
     }
 }
---
+++ movieLibrary/src/main/java/org/example/handlers/MenuHandlers.java
@@ -0,0 +1,12 @@
+package org.example.handlers;
+
+public class MenuHandlers {
+    public static void print(){
+        System.out.println("Witaj Użytkowniku w aplikacji Movie Library");
+        System.out.println("Poniżej dostępne opcje");
+        System.out.println("Wybierz jedną z nich:");
+        System.out.println("1. Wyświetl informację o randomowym filmie\n" +
+                "2. Wyświetl wszystkie filmy dla aktualnego aktora\n" +
+                "3. Wyjdź");
+    }
+}
